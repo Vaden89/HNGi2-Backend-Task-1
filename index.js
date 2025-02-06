edffb7a7a06ed20c97cc12b7aded624bb897191d
Vaden89 <isaacshosanya89@gmail.com>
2025-02-06 10:27:10 +0100

    Fixed iss

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/api/classify-number", async (req, res) => {
 
   if (!numberStr || numberStr.trim() === "") {
     return res.status(400).json({
-      number: "",
+      number: "alphabet",
       error: true,
     });
   }
